47aa16aa20ec371fe0ac82ae81ef328d27542850
zonelive-7 <mrfeiwu@gmail.com>
2026-08-08 14:54:56 +0800

    Update Doctor.java

--- src/model/Doctor.java
+++ src/model/Doctor.java
@@ -6,25 +6,45 @@ public class Doctor {
     private String specialist;
     private String workTime;
     private String qualification;
+    private int room; // 新增 PDF 要求的房间号
 
-
-    //constructor
     public Doctor() {}
 
-    public Doctor(String id, String name, String specialist, String workTime, String qualification) {
+    public Doctor(String id, String name, String specialist, String workTime, String qualification, int room) {
         this.id = id;
         this.name = name;
         this.specialist = specialist;
         this.workTime = workTime;
         this.qualification = qualification;
+        this.room = room;
     }
-    //print the doctor info
+
     public void showDoctorInfo() {
-        System.out.printf("[%s]\t[%s]\t[%s]\t[%s]\t[%s]%n",
-                id, name, specialist, workTime, qualification);
-
-
+        System.out.printf("[%s]\t[%s]\t[%s]\t[%s]\t[%s]\t[%d]%n",
+                id, name, specialist, workTime, qualification, room);
     }
 
+    public String getDoctorInfo() {
+        return String.format("ID: %s | Name: %s | Specialist: %s | WorkTime: %s | Qualification: %s | Room: %d",
+                id, name, specialist, workTime, qualification, room);
+    }
 
+    // Getters & Setters
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getSpecialist() { return specialist; }
+    public void setSpecialist(String specialist) { this.specialist = specialist; }
+
+    public String getWorkTime() { return workTime; }
+    public void setWorkTime(String workTime) { this.workTime = workTime; }
+
+    public String getQualification() { return qualification; }
+    public void setQualification(String qualification) { this.qualification = qualification; }
+
+    public int getRoom() { return room; }
+    public void setRoom(int room) { this.room = room; }
 }
